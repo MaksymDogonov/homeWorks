4c4565f3edd8fed788552f4244c3cb6eeb053922
Dogonov <dogonov1996@gmail.com>
2023-03-10 23:15:53 +0200

    hw_4

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const operation = prompt('select arithmetic operation (add, sub, mult, div)');
+const number1 = +prompt('Add number 1');
+const number2 = +prompt('Add number 2');
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+if (operation === 'add') {
+    console.log(`${number1} + ${number2} = ${addition(number1, number2)}`);
+}
+if (operation === 'sub') {
+    console.log(`${number1} - ${number2} = ${subtraction(number1, number2)}`);
+}
+if (operation === 'mult') {
+    console.log(`${number1} * ${number2} = ${multiplication(number1, number2)}`);
+}
+if (operation === 'div') {
+    console.log(`${number1} / ${number2} = ${division(number1, number2)}`);
+}
+else {
+    console.log('Only one of the listed operations')
+}
