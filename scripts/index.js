971ce79a1bc5698e8f8651a12e304bb25b377e7f
Dogonov <dogonov1996@gmail.com>
2023-03-10 23:17:07 +0200

    hw_5

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,35 @@
 'use strict'
+
+//Task #1
+const user = {
+    name: 'John',
+    surname: 'Smith',
+};
+user.name = 'Pete';
+delete user.name;
+
+// //Task #2
+// const user = {
+//     name: '«John»'
+// };
+// //Так можливо, томущо const захищая саме перемену а не тещо в середені обєкта
+// user.name = '«Pete»';
+// //так це буду працювати
+
+//Task #3
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+const array = Object.values(salaries);
+
+function arraySum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    console.log(sum);
+}
+
+arraySum(array);
