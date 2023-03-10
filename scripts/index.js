eda8d4b366e4cc28fa05b4e796b9945ff353dba5
Dogonov <dogonov1996@gmail.com>
2023-03-10 23:14:16 +0200

    hw_3

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 'use strict'
+// Task #1
+const name = "Джон";
+const admin = name;
+alert(admin);
+
+// Task #2
+const ourPlaneName = 'earth';
+const nameCurrentVisitor = 'name';
+
+//Task #3
+const result = '10' + 10;
+console.log(typeof result);
+// при складанні строки та номеру буде строка, а при вичоті JS буде намагатися перевезти строку до номеру якщо це можливо.
