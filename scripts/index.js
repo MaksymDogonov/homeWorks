edc85473e0ed3a422190bbcb5573c998864c2e26
Dogonov <dogonov1996@gmail.com>
2023-03-10 23:11:38 +0200

    hw_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
 'use strict'
+const number1 = +prompt('Add number 1');
+const number2 = +prompt('Add number 2');
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+console.log(addition(number1, number2));
+console.log(subtraction(number1, number2));
+console.log(multiplication(number1, number2));
+console.log(division(number1, number2));
