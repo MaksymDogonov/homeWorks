68e58ba62454c42f8ad689256a3fefb06fd646ae
Dogonov <dogonov1996@gmail.com>
2023-03-12 11:46:12 +0200

    hw 9 new

--- scripts/index.js
+++ scripts/index.js
@@ -4,33 +4,23 @@ let number1 = +prompt('add 1 number');
 let number2 = +prompt('add 2 number');
 
 //task 1
+
 let comparison = (a, b) => {
-    if (a === b) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
+    return a === b;
 };
-comparison(number1, number2);
+console.log(comparison(number1, number2));
 
 //task 2
+
 let comparison = (a, b) => {
-    if (a + b > 10) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
+    return a + b > 10;
 };
-comparison(number1, number2);
+console.log(comparison(number1, number2));
 
 //task 3
 
 let comparison = (a) => {
-    if (a < 0) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
+    return (a < 0);
 };
-comparison(number1);
+console.log(comparison(number1));
 
