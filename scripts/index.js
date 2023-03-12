d03e58ebf4fbb24b8c905e0dad4630b1ad5d1527
Dogonov <dogonov1996@gmail.com>
2023-03-12 14:24:37 +0200

    hw 7 new

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,22 @@
 'use strict';
 
-let number1 = +prompt('add 1 number');
-let number2 = +prompt('add 2 number');
+const number1 = +prompt('add 1 number');
+const number2 = +prompt('add 2 number');
 
 //task 1
-let comparison = (a, b) => {
+const comparison = (a, b) => {
     return a === b;
 };
 console.log(comparison(number1, number2));
 
 //task 2
-let comparison = (a, b) => {
+const comparison = (a, b) => {
     return a + b > 10;
 };
 console.log(comparison(number1, number2));
 
 //task 3
-let comparison = (a) => {
+const comparison = (a) => {
     return (a < 0);
 };
 console.log(comparison(number1));
