2810f1d75a21cc6dba29d68b88dc90c9eb883c56
MaksymDogonov <dogonov1996@gmail.com>
2023-03-14 15:20:39 +0200

    hw 8 dont finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,37 @@
 'use strict';
+
+//task 1
+const num = +prompt('add number');
+if(num <= 0 || num === null || num.trim() === ''){
+    alert('try again');
+} else {
+    alert('OK');
+}
+
+//task 2
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+// if (numOrStr === null) {
+//     console.log('вы отменили')
+// } else if (numOrStr.trim() === '') {
+//     console.log('Empty String');
+// } else if (isNaN(+numOrStr)) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
+
+switch (numOrStr) {
+    case null:
+        console.log('вы отменили');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!')
+}
