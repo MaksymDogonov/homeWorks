f97230cac06d6079565d2cee21b3b8a555f1d475
Dogonov <dogonov1996@gmail.com>
2023-03-15 00:51:46 +0200

    hw 8

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,27 @@
 'use strict';
 
 //task 1
-const num = +prompt('add number');
-if(num <= 0 || num === null || num.trim() === ''){
-    alert('try again');
-} else {
-    alert('OK');
-}
+// const num = +prompt('add number');
+// if(num <= 0 || num === null || num.trim() === ''){
+//     alert('try again');
+// } else {
+//     alert('OK');
+// }
 
 //task 2
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
-// if (numOrStr === null) {
-//     console.log('вы отменили')
-// } else if (numOrStr.trim() === '') {
-//     console.log('Empty String');
-// } else if (isNaN(+numOrStr)) {
-//     console.log(' number is Ba_NaN')
-// } else {
-//     console.log('OK!')
-// }
-
-switch (numOrStr) {
-    case null:
+switch (isNaN(+numOrStr) ? "NaN" : numOrStr === null ? "null" : numOrStr.trim().length === 0 ? "" : "number") {
+    case "null":
         console.log('вы отменили');
         break;
-    case numOrStr.trim() === '':
+    case "NaN":
+        console.log('number is Ba_NaN');
+        break;
+    case "":
         console.log('Empty String');
         break;
-    case isNaN(+numOrStr):
-        console.log(' number is Ba_NaN');
-        break;
     default:
-        console.log('OK!')
+        console.log('OK!');
 }
