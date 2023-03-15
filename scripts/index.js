089374282a74770516317d89c65faf53d1a99f53
MaksymDogonov <dogonov1996@gmail.com>
2023-03-15 14:47:33 +0200

    hw 13

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const number = prompt('add number');
+let arrOfNumber = null;
+if (isNaN(+number) || number === null || number.trim() === '') {
+    alert('add number!');
+} else {
+    arrOfNumber = Array.from(number);
+    const lastNumber = parseInt(arrOfNumber[arrOfNumber.length - 1]);
+    console.log(arrOfNumber);
+    if ([0, 5, 6, 7, 8, 9].includes(lastNumber)) {
+        alert(`${number} лет`);
+    } else if ([2, 3, 4].includes(lastNumber)) {
+        alert(`${number} года`);
+    } else {
+        alert(`${number} год`);
+    }
+};
