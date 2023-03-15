e8187a88c5e0c8f14578ad2ff5b4f5e3d986ee94
Dogonov <dogonov1996@gmail.com>
2023-03-15 17:22:37 +0200

    hw 12

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const a = {
+    x: 4,
+    y: 9
+};
+const r = 10;
+const message = 'лежит внутри окружност';
+const result = a.x ** 2 + a.y ** 2 <= r ** 2 ? alert(message) : alert(`не ${message}`);
