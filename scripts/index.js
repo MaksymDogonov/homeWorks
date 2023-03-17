38244589a8e3f306ce36fa3881b908d9db19f0ae
Dogonov <dogonov1996@gmail.com>
2023-03-17 17:05:35 +0200

    hw 15

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,5 @@
 'use strict';
-// Написати цикли, які зможуть:
-//
-// Вивести на сторінку в один рядок через кому числа від 10 до 20.
-// Вивести квадрати чисел від 10 до 20.
-// Вивести таблицю множення на 7.
-// Знайти суму всіх цілих чисел від 1 до 15.
-// Знайти добуток усіх цілих чисел від 15 до 35.
-// Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
-// Вивести суму лише парних чисел в діапазоні від 30 до 80.
-// Вивести всі числа в діапазоні від 100 до 200 кратні 3.
-//  Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
-//     Визначити кількість його парних дільників.
-//     Знайти суму його парних дільників.
-//     Надрукувати повну таблицю множення від 1 до 10.
+
 //task 1
 let result = null;
 let resultString = '';
@@ -79,7 +66,25 @@ for (let i = 100; i < 200; i++) {
     console.log(i);
 }
 
-//task 11
+//task 9 ,10 , 11
+const mathRandom = Math.floor(Math.random() * 100) + 10;
+console.log(mathRandom);
+let resultEvenNumb = null;
+let quantity = null;
+for (let i = 1; i <= mathRandom; i++) {
+    if (mathRandom % i !== 0) {
+        continue;
+    }
+    console.log(i);
+    if (i % 2 === 0) {
+        quantity += 1;
+        resultEvenNumb = resultEvenNumb += i;
+    }
+}
+console.log(`result task 10 : ${quantity}`);
+console.log(`result task 11 : ${resultEvenNumb}`);
+
+//task 12
 
 for (let i = 0; i <= 10; i++) {
     for (let j = 0; j <= 10; j++) {
