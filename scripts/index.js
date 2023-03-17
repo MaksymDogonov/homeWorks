02875957596f30a2046091d04512dc29a301e451
Dogonov <dogonov1996@gmail.com>
2023-03-18 00:13:50 +0200

    hw 15

--- scripts/index.js
+++ scripts/index.js
@@ -86,7 +86,7 @@ console.log(`result task 11 : ${resultEvenNumb}`);
 
 //task 12
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     for (let j = 0; j <= 10; j++) {
         let result = i * j;
         console.log(`${i} * ${j} = ${result}`);
