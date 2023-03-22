6514d2d83d32349ca7f4a96558e266cc87259e51
Dogonov <dogonov1996@gmail.com>
2023-03-22 20:40:25 +0200

    hw 17

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,17 @@
 'use strict';
 
-const padString = (str, num, symbol, bool) => {
-    if (str === undefined) {
-        return 'Error string';
-    } else if (num === undefined) {
-        return 'Error number';
-    } else if (symbol === undefined) {
-        return 'Error symbol';
-    } else if (bool === undefined) {
-        return 'Error boolean';
-    } else {
-        let strAddSymbol = '';
-        for (let i = str.length; i < num; i++) {
-            strAddSymbol += symbol;
-        }
-        let strSymbolAndStr = '';
-        if (bool === false) {
-            strSymbolAndStr = strSymbolAndStr.concat(strAddSymbol, str);
-        } else {
-            strSymbolAndStr = strSymbolAndStr.concat(str, strAddSymbol);
-        }
-        return strSymbolAndStr.substring(0, num);
+const padString = (str, num, symbol, bool = true) => {
+    if (typeof str !== 'string') return 'Error string';
+    if (typeof num !== 'number' || Number.isNaN(num)) return 'Error number';
+    if (typeof symbol !== 'string' || symbol.length !== 1) return 'Error symbol';
+    if (typeof bool !== 'boolean') return 'Error boolean';
+    if (str.length === num) return str;
+    if (str.length > num) return str.substring(0, num);
+
+    let strAddSymbol = '';
+    for (let i = str.length; i < num; i++) {
+        strAddSymbol += symbol;
     }
+        return bool ? str + strAddSymbol : strAddSymbol + str;
 };
 console.log(padString('home work 17', 20, '*', false));
