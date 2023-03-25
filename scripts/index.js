a0f678114d734876b9e4ac648b11963a9d343062
MaksymDogonov <dogonov1996@gmail.com>
2023-03-25 14:02:00 +0200

    hw 22

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const number = +prompt('add number');
+const lesserDivisor = (num) => {
+    if (num < 1) return NaN;
+    if (num === 1) return 1;
+    let arr = [];
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0) arr.push(i);
+    }
+    return arr[0];
+}
+console.log(lesserDivisor(number));
