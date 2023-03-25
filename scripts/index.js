42eb89f12963ef78b6e8ec0d3afc4b73d80a6f8a
MaksymDogonov <dogonov1996@gmail.com>
2023-03-25 13:46:34 +0200

    hw 21

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const number = +prompt('add number');
+const primeNumbers = (num) => {
+    if (num === 1 || num === 0) return 'false';
+    let arr = [];
+    for (let i = 0; i <= num; i++) {
+        if (num % i === 0) arr.push(i);
+    }
+    if (arr.length === 2) return 'true';
+    if (arr.length !== 2) return 'false';
+}
+console.log(primeNumbers(number));
