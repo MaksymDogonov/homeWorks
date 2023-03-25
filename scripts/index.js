391988d397f23d042acb038f6c3c2b799494268d
MaksymDogonov <dogonov1996@gmail.com>
2023-03-25 13:10:52 +0200

    hw 20

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
 'use strict';
+const fib = (n = 1) => {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+console.log(fib(15));
