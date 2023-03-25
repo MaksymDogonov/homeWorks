94ae625962e315c246a2473b812f43c110194af9
Dogonov <dogonov1996@gmail.com>
2023-03-25 23:48:04 +0200

    hw 19

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const cost = (liters) => {
-    const weight = 75;
-    const price = 13;
-    const needPot = 4;
-    let result = liters * needPot * weight/ 1000 * price;
+const cost = (liters, weight, price, needPot) => {
+    let result = liters * needPot * weight / 1000 * price;
     if (!Number.isInteger(result)) return Math.ceil(result);
     return result;
 }
-console.log(`стоимость ${cost(48)} грн`);
+console.log(`стоимость ${cost(48, 75, 13, 4)} грн`);
