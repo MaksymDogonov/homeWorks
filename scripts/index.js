e5f19f542cec313c1915de50f2fbbb881cb561e8
Dogonov <dogonov1996@gmail.com>
2023-03-25 23:43:15 +0200

    hw 21

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,11 @@
 
 const number = +prompt('add number');
 const primeNumbers = (num) => {
-    if (num === 1 || num === 0) return 'false';
-    let arr = [];
+    if (num === 1 || num === 0) return false;
+    const arr = [];
     for (let i = 0; i <= num; i++) {
         if (num % i === 0) arr.push(i);
     }
-    if (arr.length === 2) return 'true';
-    if (arr.length !== 2) return 'false';
+    return arr.length === 2;
 }
 console.log(primeNumbers(number));
