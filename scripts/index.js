2a892bb93a122e3fe3d9688d7e61fa0eeb5bad24
MaksymDogonov <dogonov1996@gmail.com>
2023-03-28 13:35:20 +0300

    hw 23

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const exponentiation = (num1, num2) => {
+    const result = num1 ** num2;
+    alert(`result exponentiation ${num1} in ${num2} = ${result}`);
+}
+const multiplay = (num1, num2) => {
+    const result = num1 * num2;
+    alert(`result multiplay ${num1} in ${num2} = ${result}`);
+}
+const division = (num1, num2) => {
+    const result = num1 / num2;
+    alert(`result division ${num1} in ${num2} = ${result}`);
+}
+const modulo = (num1, num2) => {
+    const result = num1 % num2;
+    alert(`result modulo ${num1} in ${num2} = ${result}`);
+}
+const mainFunction = (function1) => {
+    const number1 = +prompt('add number 1');
+    const number2 = +prompt('add number 2');
+    function1(number1, number2);
+}
+mainFunction(exponentiation);
+mainFunction(multiplay);
+mainFunction(division);
+mainFunction(modulo);
