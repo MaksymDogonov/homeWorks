78637bfc3b850b9c0c529b64e66cde332d29808e
Dogonov <dogonov1996@gmail.com>
2023-03-31 00:08:40 +0300

    hw 13

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const number = prompt('add number');
-let arrOfNumber = null;
 if (isNaN(+number) || number === null || number.trim() === '') {
     alert('add number!');
 } else {
-    arrOfNumber = Array.from(number);
+    let arrOfNumber = Array.from(number);
     const lastNumber = parseInt(arrOfNumber[arrOfNumber.length - 1]);
+    console.log(arrOfNumber);
     if ([0, 5, 6, 7, 8, 9].includes(lastNumber)) {
         alert(`${number} лет`);
     } else if ([2, 3, 4].includes(lastNumber)) {
@@ -15,3 +15,4 @@ if (isNaN(+number) || number === null || number.trim() === '') {
         alert(`${number} год`);
     }
 }
+;
