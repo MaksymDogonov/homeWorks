3de2db2f8dd3ce66c3253cb8883bef5b289be668
Maksym <dogonov1996@gmail.com>
2023-04-11 15:12:55 +0300

    hw 26 not full

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,28 @@
 'use strict';
+// Вам нужно написать функции indexOf, lastIndexOf, find, findIndex, includes, every и some, которые работают так же, как
+// одноименные методы массивов.
+const array = [2, 5, 9, 6];
+//indexOf
+const indexOf = (arr, element) => {
+    if (arr.includes(element) === true) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === element) {
+                return i;
+            }
+        }
+    } else {
+        return -1;
+    }
+}
+indexOf(array, 9);
+//lastIndexOf
+
+//find
+
+//findIndex
+
+//includes
+
+//every
+
+//some
