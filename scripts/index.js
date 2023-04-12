ea4740c0a90c2357a152eca316b3c596e006d350
Maksym <dogonov1996@gmail.com>
2023-04-12 15:00:33 +0300

    hw 26 not full

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,49 @@
 // одноименные методы массивов.
 const array = [2, 5, 9, 6];
 //indexOf
-const indexOf = (arr, element) => {
-    if (arr.includes(element) === true) {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] === element) {
-                return i;
-            }
+// const indexOf = (arr, element) => {
+//     if (arr.includes(element) === true) {
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] === element) {
+//                 return i;
+//             }
+//         }
+//     } else {
+//         return -1;
+//     }
+// }
+// indexOf(array, 9);
+//lastIndexOf
+// const lastIndexOf = (arr, element) => {
+//     let result;
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         if (arr[i] === element) {
+//             return result = i;
+//         }
+//     }
+//     if( result === undefined){
+//         return -1;
+//     }
+// }
+// lastIndexOf(array, 7);
+//find
+const doSomething = (element, index, arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === element) {
+            return arr[i];
         }
-    } else {
-        return -1;
     }
 }
-indexOf(array, 9);
-//lastIndexOf
+function find(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(element, i, arr)) {
+            return element;
+        }
+    }
+    return undefined;
+}
 
-//find
+find(array, doSomething(5));
 
 //findIndex
 
