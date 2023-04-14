097056f46734b3059ca653421ed323b33618e940
Maksym <dogonov1996@gmail.com>
2023-04-14 15:04:21 +0300

    hw 26 dose not has npm

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,35 @@
 'use strict';
-// Вам нужно написать функции indexOf, lastIndexOf, find, findIndex, includes, every и some, которые работают так же, как
-// одноименные методы массивов.
-const array = [2, 5, 9, 6];
+
+const array = [2, 4, 8, 6, 7 , 5 ,9];
+
 //indexOf
-// const indexOf = (arr, element) => {
-//     if (arr.includes(element) === true) {
-//         for (let i = 0; i < arr.length; i++) {
-//             if (arr[i] === element) {
-//                 return i;
-//             }
-//         }
-//     } else {
-//         return -1;
-//     }
-// }
-// indexOf(array, 9);
+const indexOf = (arr, element) => {
+    if (arr.includes(element) === true) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === element) {
+                return i;
+            }
+        }
+    } else {
+        return -1;
+    }
+}
+indexOf(array, 9);
+
 //lastIndexOf
-// const lastIndexOf = (arr, element) => {
-//     let result;
-//     for (let i = arr.length - 1; i >= 0; i--) {
-//         if (arr[i] === element) {
-//             return result = i;
-//         }
-//     }
-//     if( result === undefined){
-//         return -1;
-//     }
-// }
-// lastIndexOf(array, 7);
+const lastIndexOf = (arr, element) => {
+    let result;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === element) {
+            return result = i;
+        }
+    }
+    if( result === undefined){
+        return -1;
+    }
+}
+lastIndexOf(array, 7);
+
 //find
 const find = (arr, callback) => {
     for (let i = 0; i < arr.length; i++) {
@@ -40,9 +42,52 @@ const find = (arr, callback) => {
 find(array, (it) => it === 9);
 
 //findIndex
+const findIndex = (arr, callback) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            return i;
+        }
+    }
+    return -1;
+}
+findIndex(array, (it) => it === 6);
 
 //includes
+const includes = (arr, element) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === element) {
+            return true;
+        }
+    }
+    return false;
+}
+includes(array, 9);
 
 //every
+const every = (arr, callback) => {
+    if (arr.length === 0) {
+        return true;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            continue;
+        }
+        return false;
+    }
+    return true;
+}
+every(array, (it) => it % 2);
 
 //some
+const some = (arr, callback) => {
+    if (arr.length === 0) {
+        return false;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            return true;
+        }
+    }
+    return false;
+}
+some(array, (it) => it === 7);
