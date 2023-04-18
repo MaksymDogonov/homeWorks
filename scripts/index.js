2dedd787a5c48d6eaa49812e3ee21f0aa78876cc
Dogonov <dogonov1996@gmail.com>
2023-04-18 17:37:49 +0300

    28 hw 9/10

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 'use strict';
 // Дан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
-//     Знайти суму та кількість позитивних елементів.
-//     Знайти мінімальний елемент масиву та його порядковий номер.
-//     Знайти максимальний елемент масиву та його порядковий номер.
-//     Визначити кількість негативних елементів.
-//     Знайти кількість непарних позитивних елементів.
-//     Знайти кількість парних позитивних елементів.
-//     Знайти суму парних позитивних елементів.
-//     Знайти суму непарних позитивних елементів.
-//     Знайти добуток позитивних елементів.
-//     Знайти найбільший серед елементів масиву, ост альні обнулити.
+//     1Знайти суму та кількість позитивних елементів.
+//     2Знайти мінімальний елемент масиву та його порядковий номер.
+//     3Знайти максимальний елемент масиву та його порядковий номер.
+//     4Визначити кількість негативних елементів.
+//     5Знайти кількість непарних позитивних елементів.
+//     6Знайти кількість парних позитивних елементів.
+//     7Знайти суму парних позитивних елементів.
+//     8Знайти суму непарних позитивних елементів.
+//     9Знайти добуток позитивних елементів.
+//     0Знайти найбільший серед елементів масиву, ост альні обнулити.
 
 const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
@@ -43,39 +43,102 @@ const second = () => {
 second();
 
 const third = () => {
-    let minElement = array[0];
+    let maxElement = array[0];
     const maxElAndNumber = (element, index) => {
-        if (element > minElement) {
-            minElement = element;
+        if (element > maxElement) {
+            maxElement = element;
         }
     }
     array.forEach(maxElAndNumber);
-    const numberOfArr = array.indexOf(minElement);
+    const numberOfArr = array.indexOf(maxElement);
     console.log(`порядковий номер ${numberOfArr}`);
-    console.log(`максимальний елемент масиву ${minElement}`);
+    console.log(`максимальний елемент масиву ${maxElement}`);
 }
 third();
 
 const fourth = () => {
     let counter = 0;
-    const sumAndQun = (element, index) => {
+    const qunNeg = (element, index) => {
         if (element < 0) {
             counter += 1;
         }
     }
-    array.forEach(sumAndQun);
+    array.forEach(qunNeg);
     console.log(`кількість ${counter}`);
 }
 fourth();
 
 const fifth = () => {
     let counter = 0;
-    const sumAndQun = (element, index) => {
+    const qunPos = (element, index) => {
         if (element > 0 && element % 2 === 1) {
             counter += 1;
         }
     }
-    array.forEach(sumAndQun);
+    array.forEach(qunPos);
     console.log(`кількість ${counter}`);
 }
 fifth();
+
+const sixth = () => {
+    let counter = 0;
+    const qunEvenPos = (element, index) => {
+        if (element > 0 && element % 2 === 0) {
+            counter += 1;
+        }
+    }
+    array.forEach(qunEvenPos);
+    console.log(`кількість ${counter}`);
+}
+sixth();
+
+const seventh = () => {
+    let sum = 0;
+    const sumEvenPos = (element, index) => {
+        if (element > 0 && element % 2 === 0) {
+            sum += element;
+        }
+    }
+    array.forEach(sumEvenPos);
+    console.log(`sum ${sum}`);
+}
+seventh();
+
+const eighth = () => {
+    let sum = 0;
+    const sumNotEvenPos = (element, index) => {
+        if (element > 0 && element % 2 === 1) {
+            sum += element;
+        }
+    }
+    array.forEach(sumNotEvenPos);
+    console.log(`sum ${sum}`);
+}
+eighth();
+
+const ninth = () => {
+    let mul = 1;
+    const mulEvenPos = (element, index) => {
+        if (element > 0) {
+            mul *= element;
+        }
+    }
+    array.forEach(mulEvenPos);
+    console.log(`mul ${mul}`);
+}
+ninth();
+
+const tenth = () => {
+    let maxElement = array[0];
+    const arr = [];
+    const maxNumber = (element, index) => {
+        if (element > maxElement) {
+            maxElement = element;
+            arr.push(element);
+        }
+    }
+    array.forEach(maxNumber);
+    console.log(`максимальний елемент масиву ${arr[arr.length - 1]}`);
+    console.log(arr)
+}
+tenth();
