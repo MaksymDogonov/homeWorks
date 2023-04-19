47dc145cb693707ff7604ee266ec8e039be473fb
Maksym <dogonov1996@gmail.com>
2023-04-19 14:48:21 +0300

    hw 29

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const users = [{
+    "index": 0,
+    "isActive": true,
+    "balance": "$2,226.60",
+    "name": "Eugenia Sawyer",
+    "gender": "female",
+    "phone": "+1 (840) 583-3207",
+    "address": "949 John Street, Rose, Puerto Rico, 1857"
+}, {
+    "index": 1,
+    "isActive": true,
+    "balance": "$2,613.77",
+    "name": "Pauline Gallegos",
+    "gender": "female",
+    "phone": "+1 (985) 593-3328",
+    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+}, {
+    "index": 2,
+    "isActive": false,
+    "balance": "$3,976.41",
+    "name": "Middleton Chaney",
+    "gender": "male",
+    "phone": "+1 (995) 591-2478",
+    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+}, {
+    "index": 3,
+    "isActive": true,
+    "balance": "$1,934.58",
+    "name": "Burns Poole",
+    "gender": "male",
+    "phone": "+1 (885) 559-3422",
+    "address": "730 Seba Avenue, Osage, Alabama, 6290"
+}, {
+    "index": 4,
+    "isActive": true,
+    "balance": "$3,261.65",
+    "name": "Mcfadden Horne",
+    "gender": "male",
+    "phone": "+1 (942) 565-3988",
+    "address": "120 Scholes Street, Kirk, Michigan, 1018"
+}, {
+    "index": 5,
+    "isActive": false,
+    "balance": "$1,790.56",
+    "name": "Suzette Lewis",
+    "gender": "female",
+    "phone": "+1 (837) 586-3283",
+    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+}]
+
+function usersChecks(users) {
+    const usersBalanceMore2000 = [];
+    let sum = 0
+    for (let user of users) {
+        let temp = user.balance.replace(/[^0-9.]/g, '');
+        sum += temp * 100;
+        if (temp > 2000) {
+            usersBalanceMore2000.push(user.phone);
+        }
+    }
+    sum = sum / 100;
+    return [sum, usersBalanceMore2000];
+}
+const usersResults = usersChecks(users);
+console.log(`масив телефонних номерів користувачів балангс яких більше 2000: ${usersResults[1]}`);
+console.log(`сумма користувачів: ${usersResults[0]}`);
