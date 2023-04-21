ac6eb6eea563e8102179993f06e9f28aeb76c661
Dogonov <dogonov1996@gmail.com>
2023-04-21 20:49:46 +0300

    hw 31

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const palindrome = (number, steps = 0) => {
+    const elReverse = +Array.from(String(number)).reverse().join('');
+    if (number === elReverse) return {result: elReverse, steps};
+    return palindrome(number + elReverse, steps +1);
+}
+console.log(palindrome(96))
