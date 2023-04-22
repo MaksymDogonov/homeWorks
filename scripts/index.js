5475709d69d6362be0be21ff03bfb34920ee3f47
Maksym <dogonov1996@gmail.com>
2023-04-22 14:53:13 +0300

    hw 32 not full

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
 'use strict';
+const array = [1, 2, 3, [1, 2]];
+const resultArr = [];
+const flat = (arr) => {
+    if (flat.length !== 1) throw new Error('Function accepts only 1 argument, too much arguments provided');
+
+    if (arr.every(item => Array.isArray(item))) console.log(resultArr === arr);
+
+    // if (arr[i]) resultArr = arr;
+}
+flat(array,2);
