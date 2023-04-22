322c352c9c21e5ee09ca58c9863e769d9b243dc9
Dogonov <dogonov1996@gmail.com>
2023-04-22 20:32:52 +0300

    hw 32

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
 'use strict';
-const array = [1, 2, 3, [1, 2]];
-const resultArr = [];
-const flat = (arr) => {
-    if (flat.length !== 1) throw new Error('Function accepts only 1 argument, too much arguments provided');
 
-    if (arr.every(item => Array.isArray(item))) console.log(resultArr === arr);
-
-    // if (arr[i]) resultArr = arr;
+const array = [1, 2, 3, [1, 2, [25, 5]]];
+const flat = (...args) => {
+    if (args.length !== 1) throw new Error('Function accepts only 1 argument, too much arguments provided');
+    const resultArray = [];
+    args[0].forEach((it) => {
+        if (Array.isArray(it)) {
+            resultArray.push(...flat(it));
+        } else {
+            resultArray.push(it);
+        }
+    })
+    return resultArray;
 }
-flat(array,2);
+console.log(flat(array));
