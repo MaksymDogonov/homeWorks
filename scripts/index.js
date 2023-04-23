71703e3d7b1eb72ac74fe88acc1663f01f18c4d3
Dogonov <dogonov1996@gmail.com>
2023-04-23 11:47:27 +0300

    hw 34 not full

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+void function () {
+    const arrOfNumber = [];
+    const randomNumGenerator = () => {
+        const numberGenerator = () => {
+            if(arrOfNumber.length === 100) return NaN;
+        }
+            const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+            if (arrOfNumber.includes(randomNumber)) {
+                return numberGenerator;
+            }
+            arrOfNumber.push(randomNumber);
+            return randomNumber;
+        }
+        return randomNumGenerator;
+    }
+    const result = randomNumGenerator();
+    console.log(result);
+    let res = null;
+    for (let i = 0;)
