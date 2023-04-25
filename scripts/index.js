9397270e644192ed600dd91b8f0ed31b7d7f276f
Maksym <dogonov1996@gmail.com>
2023-04-25 15:08:05 +0300

    hw 37

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function bind(fn, context, ...rest) {
+    return function(...args) {
+        const uuid = Date.now().toString();
+        context[uuid] = fn;
+        const res = context[uuid](...rest, ...args);
+        delete context[uuid];
+        return res;
+    }
+}
+
+
+
+const user = {
+    firstName: '',
+    lastName: '',
+    fullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+function getFullName(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    return this.fullName();
+}
+
+bind(getFullName, user, 'Иван', 'Человеков')() // => Иван Человеков
+bind(getFullName, user, 'Иван')('Человеков') // => Иван Человеков
+bind(getFullName, user)('Иван', 'Человеков') // => Иван Человеков
