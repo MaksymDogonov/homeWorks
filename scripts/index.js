0561655f34eb2f52780280be9557e9832981a616
Dogonov <dogonov1996@gmail.com>
2023-04-25 18:07:48 +0300

    hw 33

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// Вам нужно написать функцию, которая принимает один параметр. При первом вызове она его запоминает, при втором —
-// суммирует переданный параметр с тем, что передали первый раз и тд. Например:
-//
-// sum(3) = 3;
-// sum(5) = 8;
-
 const sum = (number) => {
     let result = 0;
     const plus = number;
@@ -13,12 +7,8 @@ const sum = (number) => {
         result += plus;
         return result;
     }
-    return save;
-}
-const result = sum(5);
-const result2 = sum(3);
-console.log(result());
-console.log(result2());
-console.log(result());
-console.log(result());
+};
+const sum = fn();
+console.log(sum(5));
+console.log(sum(3));
 
