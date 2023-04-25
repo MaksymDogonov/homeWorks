c49f2188d91a6250259033f17f4043222f86c485
Maksym <dogonov1996@gmail.com>
2023-04-25 14:21:37 +0300

    hw 36

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let calculator = {
+    read: () => {
+        this.element1 = +prompt('add element 1');
+        this.element2 = +prompt('add element 2');
+        if (isNaN(this.element1) || isNaN(this.element2)) {
+            alert('add number');
+        }
+    },
+    sum: () => {return this.element1 + this.element2},
+    mul: () => {return this.element1 * this.element2}
+};
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
