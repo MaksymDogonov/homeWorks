13e2be03b1034b71c5a2dda4914feefcf995072a
Dogonov <dogonov1996@gmail.com>
2023-04-26 17:39:09 +0300

    hw 33

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const fn = () => {
+const saveAndSum = () => {
     let result = 0;
     return (number) => {
         result += number;
         return result;
     }
 };
-const sum = fn();
+const sum = saveAndSum();
 console.log(sum(5));
 console.log(sum(3));
 
