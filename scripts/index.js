f18e8417790486fb4272dacbba0b20fe4c737fa5
Dogonov <dogonov1996@gmail.com>
2023-04-26 19:13:34 +0300

    hw 35

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,36 @@
 'use strict';
 
-// Создайте функцию counter, которая будет создавать счетчик и иметь один необязательный параметр типа number. Вызов
-// функции должен возвращать объект (счетчик) с тремя методами: increase, decrease, value.
-//
-//     Вызов метода value должен возвращать текущее значение счетчика, increse – увеличивать его на 1, а decrease – уменьшать
-// на 1.
-//
-// Вызов функции counter должен создавать независимый счетчик – это значит, что вызов методов одного счетчика никак не
-// должен влиять на любой другой счетчик.
-//
-//     Дополнительно - Если хотите оценку 100 баллов
-//
-// Необходимо подсчитывать сколько раз были вызваны функции (increase, decrease, get)
-// Добавить метод getStatistic который вернет нам объект с данными которые мы подсчитывали в пункте выше
-// Добавить метод который будет обнулять наш счетчик
-
-const counter = (...rest) => {
-    const meter = {
-        value: 0,
-        increase: 0,
-        get: 0
+const counter = (value = 0) => {
+    let count = value;
+    const statistic = {
+        counterValue: 0,
+        counterDecrease: 0,
+        counterIncrease: 0
     }
-    let count = 0;
-    const makeCounter = (number = 0) => {
-        if (number === 0)
-            return count++;
-        else
-            count = number;
-        return count;
+    return {
+        value() {
+            statistic.counterValue++;
+            return count;
+        },
+        increase() {
+            statistic.counterIncrease++;
+            count += 1
+        },
+        decrease() {
+            statistic.counterDecrease++;
+            count -= 1
+        },
+        getStatistic() {
+            return statistic;
+        },
+        reset() {
+            statistic.counterValue = 0;
+            statistic.counterDecrease = 0;
+            statistic.counterIncrease = 0;
+            count = value;
+        }
     }
-    meter.value = makeCounter();
-    meter.increase = makeCounter() + 1;
-    meter.get = makeCounter() - 1;
-    return meter;
 }
+
 const result = counter();
-
-console.log(result);
-console.log(result.value);
-console.log(result.increase);
-console.log(result.get);
-console.log(result.increase);
-
-
-
-
-
+const result2 = counter(10);
