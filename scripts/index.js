bda0efc9f8b3662cfa1de9381bae429fe7311033
MaksymDogonov <dogonov1996@gmail.com>
2023-05-09 20:56:11 +0300

    hw 42

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
-'use strict';
+"use strict";
+
+const generateList = (array) => {
+  const ul = document.createElement("ul");
+
+  array.forEach((el) => {
+    const li = document.createElement("li");
+    let childEl;
+
+    if (Array.isArray(el)) {
+      childEl = generateList(el);
+    } else {
+      childEl = document.createTextNode(el);
+    }
+    li.appendChild(childEl);
+    ul.appendChild(li);
+  });
+  return ul;
+};
+const result = generateList([1, 2, [1.1, 1.2, 1.3], 3]);
+document.body.appendChild(result);
