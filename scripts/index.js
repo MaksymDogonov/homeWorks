5233ea62ea75d2559223c74cae0c34a1c8efca1d
MaksymDogonov <dogonov1996@gmail.com>
2023-05-10 18:24:06 +0300

    hw 43

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+const createTable = () => {
+  const tableElement = document.createElement("table");
+  for (let i = 1; i <= 10; i++) {
+    const rowElement = document.createElement("tr");
+    for (let j = 1; j <= 10; j++) {
+      const cellElement = document.createElement("td");
+      cellElement.innerText = `${i * j}`;
+      rowElement.appendChild(cellElement);
+    }
+    tableElement.appendChild(rowElement);
+  }
+  return tableElement;
+};
+const result = createTable();
+document.body.appendChild(result);
