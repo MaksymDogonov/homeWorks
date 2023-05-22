0b9ffdc0218417b952e85339b93f10df7524269f
MaksymDogonov <dogonov1996@gmail.com>
2023-05-23 00:33:57 +0300

    hw 50

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+export const INFO_KEY = "formInfo";
+
+void (function () {
+  const form = document.querySelector("#todoForm");
+  const inputs = form.querySelectorAll("input:not([type=reset]), textarea ");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const info = {};
+
+    inputs.forEach(({ name, value }) => (info[name] = value));
+    localStorage.setItem(INFO_KEY, JSON.stringify(info));
+  });
+})();
