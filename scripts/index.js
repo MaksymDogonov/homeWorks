272b7a4e50ad31f3db2df197d60e5b1d799ab9a7
MaksymDogonov <dogonov1996@gmail.com>
2023-05-24 01:05:20 +0300

    hw 50

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,52 @@
 "use strict";
 
-export const INFO_KEY = "formInfo";
-
 void (function () {
   const form = document.querySelector("#todoForm");
-  const inputs = form.querySelectorAll("input:not([type=reset]), textarea ");
+  const divForNewCards = document.querySelector("#todoItems");
+  const inputs = Array.from(
+    form.querySelectorAll("input:not([type=reset]), textarea ")
+  );
+  const INFO_KEY = "formInfo";
+
+  const createTodoItem = ({ title, description }) => {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `<div class='col-4'>
+                          <div class='taskWrapper'>
+                            <div class='taskHeading'>${title}</div>
+                            <div class='taskDescription'>${description}</div>
+                          </div>
+                         </div>`;
+    divForNewCards.appendChild(wrapper); //
+    return wrapper; //
+  };
+
+  const getInfo = () => {
+    const data = JSON.parse(localStorage.getItem(INFO_KEY));
+    if (!data) return [];
+    return data;
+  };
+
+  const saveData = getInfo(data);
+
+  createTodoItem(saveData);
+
+  const loadedInfo = () => {
+    const data = getInfo();
+    if (!data.length) return;
+
+    data.forEach((todoItem) => {
+      const temp = createTodoItem(todoItem);
+      createTodoItem(temp);
+    });
+    document.removeEventListener(loadedInfo);
+  };
+
+  const saveTodoItem = (dataSave) => {
+    const tempData = getInfo();
+    tempData.push(dataSave);
+    localStorage.setItem(INFO_KEY, JSON.stringify(tempData));
+    return getInfo().at(-1);
+  };
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -13,5 +55,7 @@ void (function () {
 
     inputs.forEach(({ name, value }) => (info[name] = value));
     localStorage.setItem(INFO_KEY, JSON.stringify(info));
+    return info;
   });
+  document.addEventListener(loadedInfo);
 })();
