fe1e71971f7ee56efc0d957bb0efe1183955b218
MaksymDogonov <dogonov1996@gmail.com>
2023-06-15 23:34:55 +0300

    hw55

---
+++ js/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+(function () {
+  function customCreateTemplate({ title, description, id }) {
+    const template = document.createElement("div");
+    template.className = "col-6";
+    template.setAttribute("data-id", id);
+
+    template.innerHTML = `
+                        <div class="task">
+                            <div class="task__heading">${title} <sub><b>${id}</sub></b></div>
+                            <div class="task__description">${description}</div>
+                            <hr>
+                            <button class='delete' onclick='Controller.delete(${id})'>Delete</button>
+                            <div>By Maksym Dogonov </div>
+                        </div>`;
+    return template;
+  }
+
+  View.init(customCreateTemplate);
+  Model.init(localStorage, "todo-list-data");
+  Controller.init("#todoForm", "[data-todo-items]");
+})();
