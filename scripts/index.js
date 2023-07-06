969f1cd16a17b350f68de7157573dec28fdc9a04
MaksymDogonov <dogonov1996@gmail.com>
2023-07-06 16:08:32 +0300

    hw70

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+(function () {
+  const player1 = { id: 1, name: "Иван", level: 10 };
+
+  const addPlayerToLevelSet = (obj) => {
+    const weakSet = new WeakSet();
+    weakSet.add(obj);
+    return weakSet;
+  };
+  console.log(addPlayerToLevelSet(player1));
+})();
