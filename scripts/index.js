dd74a2971668cb789b2588d20907b2fafdb2d696
MaksymDogonov <dogonov1996@gmail.com>
2023-07-06 15:50:03 +0300

    hw68

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict";
+(function () {
+  const string = "abracadabra";
+
+  const uniqSymbolsCount = (str) => {
+    const set = new Set();
+
+    for (let i = 0; i < str.length; i++) {
+      set.add(str[i]);
+    }
+    return set.size;
+  };
+  console.log(uniqSymbolsCount(string));
+})();
