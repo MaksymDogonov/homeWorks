5511d790ab68e7c6a507c134e6affc85a25ac811
MaksymDogonov <dogonov1996@gmail.com>
2023-07-06 15:59:48 +0300

    hw69

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+(function () {
+  const student = { name: "Иван Иванов" };
+  const grades = { math: 90, fiz: 85, eng: 95 };
+
+  const saveGrades = (student, grades) => {
+    const weakMap = new WeakMap();
+    weakMap.set(student, grades);
+    return weakMap;
+  };
+  console.log(saveGrades(student, grades));
+})();
