19d81a521d1bbbcc3f122b620b074778f56388fc
MaksymDogonov <dogonov1996@gmail.com>
2023-07-06 15:45:16 +0300

    hw67

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
-'use strict';
+"use strict";
+(function () {
+  const goods = [
+    ["футболка", 15],
+    ["шорты", 25],
+    ["носки", 5],
+  ];
+
+  const totalCost = (arr) => {
+    const map = new Map();
+    let total = 0;
+
+    for (let [item, price] of arr) {
+      map.set(item, price);
+    }
+
+    map.forEach((value) => (total += value));
+    return total;
+  };
+  console.log(totalCost(goods));
+})();
