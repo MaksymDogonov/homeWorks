685fbe9481783c98ba8b97fb62430c99acad2bb0
MaksymDogonov <dogonov1996@gmail.com>
2023-07-09 11:36:34 +0300

    hw71

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
-'use strict';
+"use strict";
+(function () {
+  const students = [
+    { name: "John", age: 20, subject: "Math" },
+    { name: "Alice", age: 22, subject: "English" },
+    { name: "Bob", age: 21, subject: "History" },
+  ];
+
+  const printGreetings = (arr) => {
+    let stings = [];
+    let result = "";
+
+    for (let { name, subject } of arr) {
+      const sting = `Привет, ${name}! Ты изучаешь предмет ${subject}`;
+      stings.push(sting);
+      result = stings.join("\n");
+    }
+    return result;
+  };
+
+  console.log(printGreetings(students));
+})();
