26aa1055c476abc04b574ff7129771ee7771dff3
MaksymDogonov <dogonov1996@gmail.com>
2023-07-12 19:11:54 +0300

    gulp

--- react-course-1-lesson-6-main/lesson-files/gulpfile [browser-sync-watch].js
+++
@@ -1,8 +0,0 @@
-const browserSyncJob = () => {
-    browserSync.init({
-        server: "build/"
-    });
-
-    watch('dist/sass/*.scss', buildSass);
-    watch('dist/pages/*.pug', buildPug);
-};
--- react-course-1-lesson-6-main/lesson-files/gulpfile [browser-sync].js
+++
@@ -1,9 +0,0 @@
-const browserSync = require('browser-sync').create();
-
-const browserSyncJob = () => {
-    browserSync.init({
-        server: "build/"
-    });
-};
-
-exports.server = browserSyncJob;
