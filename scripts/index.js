49df5f239ef6c3ccab91d65d9fa6d5f0d576b764
MaksymDogonov <dogonov1996@gmail.com>
2023-07-16 20:44:29 +0300

    hw61

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
-'use strict';
+const parallel = require('../gulpfile.js').default;
+
+parallel();
+
+const parallel2 = require('../gulpfile.js').layoutCompile;
+
+parallel2();
+
+const assetsOptimize = require('../gulpfile.js').assetsOptimize;
+
+assetsOptimize(() => {
+    console.log('done');
+});
