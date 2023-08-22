7b66b95dcda2b13968430503a654e2304428ce4f
MaksymDogonov <dogonov1996@gmail.com>
2023-08-22 16:02:52 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Component from "./components/Component";
 
-const SomeComponent = props => {
-  return (<div>{props.name}</div>);
-}
-
-function SomeComponent(props) {
-  return (<div>{props.name}</div>);
-}
-
-class SomeComponent extends React.Component {
+class App extends React.Component {
   render() {
-    return (<div>{this.props.name}</div>)
+    return (
+        <Component/>
+    )
   }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
