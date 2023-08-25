2356ba661d06514d790ac6307ad7b872a6c5a689
MaksymDogonov <dogonov1996@gmail.com>
2023-08-25 15:54:37 +0300

    hw 51

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 'use strict';
+const openSidebarMenu = () => {
+    const buttonMenu = document.querySelector('.header__sidebar-btn');
+    const leftMenu = document.querySelector('.sidebar');
+
+    buttonMenu.addEventListener('click', (e) => {
+        if (leftMenu.classList.contains('active')) {
+            leftMenu.classList.remove('active');
+            buttonMenu.classList.remove('active');
+            leftMenu.style.display = 'none';
+        } else {
+            leftMenu.style.display = 'flex';
+            leftMenu.classList.add('active');
+            buttonMenu.classList.add('active');
+        }
+    })
+}
+openSidebarMenu();
