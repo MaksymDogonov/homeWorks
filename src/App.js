d417b15f15e5bc1403e2feba9f0004b4a560e290
MaksymDogonov <dogonov1996@gmail.com>
2023-09-23 03:48:43 +0300

    hw 93

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react"
 import TodoBox from "./components/TodoBox";
 
-class App extends React.Component {
-  render() {
-    return (
-        <TodoBox/>
-    )
-  }
-}
+const App = () => {
+  return (
+      <TodoBox />
+  );
+};
 
 export default App;
