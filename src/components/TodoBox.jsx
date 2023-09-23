0fe570508f8434bc17d547863e1c0f9c376c4e52
MaksymDogonov <dogonov1996@gmail.com>
2023-09-23 03:32:17 +0300

    hw 93

--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -1,58 +1,49 @@
-import React from "react"
-import {v4 as uuidv4} from 'uuid';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Formik, Form, Field } from 'formik';
 import Item from "./Item";
-import {Field, Form, Formik} from "formik";
 
-class TodoBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tasks: []
-        };
-    }
+const TodoBox = () => {
+    const [tasks, setTasks] = useState([]);
 
-    clickAdd = (values) => {
-        this.setState((state) => ({
-            tasks: [{
+    const clickAdd = (values) => {
+        setTasks((prevTasks) => [
+            {
                 uuid: uuidv4(),
-                value: values.task
-            }, ...state.tasks],
-        }))
+                value: values.task,
+            },
+            ...prevTasks,
+        ]);
     };
 
-    clickDelete = (uuid) => {
-        this.setState(({tasks}) => ({
-            tasks: tasks.filter((it) => it.uuid !== uuid)
-        }));
+    const clickDelete = (uuid) => {
+        setTasks((prevTasks) => prevTasks.filter((task) => task.uuid !== uuid));
     };
 
-    render = () => {
-        return (
-            <div>
-                <div className="mb-3">
-                    <Formik initialValues={{task: ''}} onSubmit={this.clickAdd}>
-                        <Form className="d-flex">
-                            <Field id='task'
-                                   name='task'
-                                   placeholder="I am going..."
-                                   className="form-control me-3"
-                            />
-                            <button type="submit" className="btn btn-primary">add</button>
-                        </Form>
-                    </Formik>
-                </div>
-                <div>
-                    {this.state.tasks.map((task) => {
-                        return <Item key={task.uuid}
-                                     task={task.value}
-                                     onRemove={() => this.clickDelete(task.uuid)}/>
-                    })}
-                </div>
+    return (
+        <div>
+            <div className="mb-3">
+                <Formik initialValues={{ task: '' }} onSubmit={clickAdd}>
+                    <Form className="d-flex">
+                        <Field
+                            id="task"
+                            name="task"
+                            placeholder="I am going..."
+                            className="form-control me-3"
+                        />
+                        <button type="submit" className="btn btn-primary">
+                            add
+                        </button>
+                    </Form>
+                </Formik>
             </div>
-        )
-    }
-
-
-}
+            <div>
+                {tasks.map((task) => (
+                    <Item key={task.uuid} task={task.value} onRemove={() => clickDelete(task.uuid)} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default TodoBox;
